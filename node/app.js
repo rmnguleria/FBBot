1bffdee87877fc331291fa7eaba78b31fef42a56
Raman Guleria <rguleria@expedia.com>
2017-01-01 11:58:31 +0530

    Tryin a new bot

--- node/app.js
+++ node/app.js
@@ -522,7 +522,9 @@ function sendTextMessage(recipientId, messageText) {
   console.log('messagetext',messageText);
   var headers = {
     'Host': 'kakko.pandorabots.com',
-    'Connection': 'keep-alive'
+    'Connection': 'keep-alive',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
   };
 
   var dataString = 'input='+messageText+'&botid=9fa364f2fe345a10&custid=9019d8ee1e0a178c';
